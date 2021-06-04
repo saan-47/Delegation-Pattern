ad2a380012a4c5efbcaf462ed6657b677910987c
Hassan <syed.hassan.ibrahim53@gmail.com>
2021-06-05 00:25:06 +0500

    Delegation pattern commit 1

---
+++ src/Main.java
@@ -0,0 +1,124 @@
+import com.sun.source.tree.WhileLoopTree;
+
+public class Main {
+
+
+
+    public static void main(String[] args) {
+//        int[] a = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+//
+//        int sum1 = 0;
+//        for (int i = 0; i < a.length; i++) {
+//            sum1 += a[i];
+//        }
+//
+//        int sum2 = 0;
+//
+//        for (int j = 0; j < a.length; j++) {
+//            sum2 += a[j];
+//
+//        }
+//        System.out.println(sum1 - sum2);
+//        Cow cow = new Cow();
+//        cow.vocalize();
+//        Dog dog  =new Dog();
+//        dog.vocalize();
+//
+//
+//        String andr = "Andrew";
+//        String b = "Andrew";
+//
+//        if(andr.equals(b)){   //checking the equality of objects using equals() methods
+//            System.out.println("andr & b are equal variables, and their respective hashvalues are:" + " "+ andr.hashCode() + " & " + b.hashCode());
+//
+//        }
+//
+//        String c = "Maria";
+//        String d= "Julie";
+//
+//        if(!c.equals(d)){    //checking  the equality of objects using equals() method
+//            System.out.println("\nc & d are Un-equal variables, and their respective hashvalues are:" + " "+ c.hashCode() + " & " + d.hashCode());
+//
+//        }
+
+        Employee employee = new Employee(new FastCoder(), new GoodDesigner());
+        employee.code();
+        employee.design();
+    }
+}
+
+interface Animal
+{
+    void vocalize();
+}
+class Dog implements Animal {
+    public void vocalize() { System.out.println("Woof!"); }
+}
+class Cow implements Animal {
+    public void vocalize() { moo(); }
+    public void moo() { System.out.println("Moo!"); }
+}
+interface Vehicle{
+    int speed = 200;
+    int limit = 198;
+
+    //@invariant speed < limit;
+    abstract void brake();
+
+}
+class Car implements Vehicle{
+
+    int fuel;
+    boolean engineOn;
+    @Override
+    public void brake() {
+        if (speed > limit)  {
+            System.out.printf("Brakes");
+        }
+    }
+}
+
+interface WhoCanCode{
+    void code();
+}
+interface WhoCanDesign{
+    void design();
+}
+
+class Employee implements WhoCanCode, WhoCanDesign{
+
+    WhoCanCode whoCanCode;
+    WhoCanDesign whoCanDesign;
+
+    public Employee(WhoCanCode whoCanCode, WhoCanDesign whoCanDesign) {
+        this.whoCanCode = whoCanCode;
+        this.whoCanDesign = whoCanDesign;
+    }
+
+    @Override
+    public void design() {
+        whoCanCode.code();
+    }
+
+    @Override
+    public void code() {
+        whoCanDesign.design();
+    }
+}
+
+class FastCoder implements WhoCanCode{
+    @Override
+    public void code() {
+        System.out.println("Coded by fastcoder");
+    }
+}
+class GoodDesigner implements WhoCanDesign{
+    @Override
+    public void design() {
+        System.out.println("Who can Design Good");
+    }
+}
+
+//public enum Planet{
+//    MERCURY(3.4580, 4.5723), VENUS(1.E452, 2.484), MARS(5.104,6.157), JUPYTER(3.274,0.6897), NEPTUNE(4.697,3.214), URANUS(1.234,3.654), SATURN(93.41,4.158), EARTH(2.215,34.654)
+//}
